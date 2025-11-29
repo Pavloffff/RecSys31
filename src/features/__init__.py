1433e09e2ad51957caaa247093f46c0c83d48620
Mikhail-cWc <misha-kopylov@bk.ru>
2025-11-29 03:27:22 +0300

    Remove main script

--- src/features/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Модули для создания признаков пользователей.
-"""
-
-from .feature_engineering import create_user_features
-
-__all__ = ['create_user_features']
-
--- src/preprocessing/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Модули для предобработки данных.
-"""
-
-from .preprocessing import merge_events_with_items
-
-__all__ = ['merge_events_with_items']
-
